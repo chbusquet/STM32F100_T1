76b79e6b62384870485af8a2ad6e704d01408511
Christian Busquet <cbusquet@yahoo.fr>
2024-09-09 21:52:26 +0200

    Ajout compteur global pour valider les lives expressions

--- App/HeartBeat/HeartBeat.c
+++ App/HeartBeat/HeartBeat.c
@@ -11,6 +11,8 @@
 
 #define LED_DELAY	1000	// in Ticks
 
+uint32_t counter = 0;
+
 void HeartBeat_(void *args)
 {
 	HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
@@ -20,5 +22,6 @@ void HeartBeat_(void *args)
 		osDelay(LED_DELAY);
 		HAL_GPIO_TogglePin(LD4_GPIO_Port, LD4_Pin);
 		HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+		counter ++;
 	}
 }
